0320f531aad1b31720334d9c7640056de41d715d
OTv1nta <mykhailo.shyriavskyi@nure.ua>
2022-06-29 23:47:47 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryRefs = document.querySelector(".gallery");
+galleryRefs.addEventListener("click", onShowBigImage);
+
+(function createMarkup() {
+  const itemMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join("");
+  galleryRefs.insertAdjacentHTML("beforeend", itemMarkup);
+})();
+
+function onShowBigImage(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let bigImageSrc = e.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `<img src="${bigImageSrc}" width="800" height="600">`
+  );
+  modal.show();
+
+  if (modal.visible()) {
+    window.addEventListener("keydown", onPressKeyESC);
+  }
+
+  function onPressKeyESC(e) {
+    if (e.code === "Escape") {
+      modal.close();
+      window.removeEventListener("keydown", onPressKeyESC);
+    }
+  }
+}
